2a771e2fc2eb54660bf052c08bfba73152f3fea7
KaoImin <kaoimin@qq.com>
2020-07-08 12:11:12 +0800

    register multi_signature service

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use admission_control::AdmissionControlService;
 use asset::AssetService;
 use authorization::AuthorizationService;
 use derive_more::{Display, From};
+use multi_signature::MultiSignatureService;
 use governance::GovernanceService;
 use kyc::KycService;
 use metadata::MetadataService;
@@ -24,6 +25,7 @@ impl ServiceMapping for DefaultServiceMapping {
             "asset" => Box::new(AssetService::new(sdk)) as Box<dyn Service>,
             "metadata" => Box::new(MetadataService::new(sdk)) as Box<dyn Service>,
             "kyc" => Box::new(KycService::new(sdk)) as Box<dyn Service>,
+            "multi_signature" => Box::new(MultiSignatureService::new(sdk)) as Box<dyn Service>,
             "riscv" => Box::new(RiscvService::init(sdk)) as Box<dyn Service>,
             "governance" => Box::new(GovernanceService::new(sdk)) as Box<dyn Service>,
             "admission_control" => Box::new(AdmissionControlService::new(sdk)) as Box<dyn Service>,
@@ -45,6 +47,7 @@ impl ServiceMapping for DefaultServiceMapping {
             "asset".to_owned(),
             "metadata".to_owned(),
             "kyc".to_owned(),
+            "multi_signature".to_owned(),
             "riscv".to_owned(),
             "governance".to_owned(),
             "admission_control".to_owned(),
